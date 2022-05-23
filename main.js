d3914c1da5ebdbc40a9353ec8e4c2bca71a7064d
Cristian Gimenez <94946341+Crisgimenez07@users.noreply.github.com>
2022-05-23 13:31:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function botaoassistir() {
+    window.open('https://youtu.be/flYJj85aJsw')
+}
+function botaoinfo() {
+    window.open('https://dragon-ball-superonline.blogspot.com/')
+}
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
